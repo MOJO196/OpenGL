7318d9de85313f34c6bbdf552faa1c8e2dee01db
MOJO196 <movhet@gmail.com>
2022-03-09 11:28:30 +0100

    fps count

--- src/Main.cpp
+++ src/Main.cpp
@@ -6,6 +6,7 @@
 #include <GLFW/glfw3.h>
 #include <gl/GL.h>
 #include <sstream>
+#include <chrono>
 
 int main()
 {
@@ -16,7 +17,7 @@ int main()
 
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(1024, 1024, "OpenGL Test", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(512, 512, "OpenGL Test", NULL, NULL);
 
 	if (window == NULL)
 	{
@@ -31,7 +32,7 @@ int main()
 
 	glDebugMessageCallback(openGLDebugCallback, 0);
 
-	glViewport(0, 0, 1024, 1024);
+	glViewport(0, 0, 512, 512);
 
 	GLuint program = LoadShader("src/shader/vertexShaderSource.glsl", "src/shader/fragmentShaderSource.glsl");
 	glUseProgram(program);
@@ -51,7 +52,7 @@ int main()
 		 0.0f,  0.5f * float(sqrt(3)) * 2 / 3, 0.0f,     1.0f, 1.0f, 1.0f, // Upper corner
 		-0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.0f, 0.0f, 1.0f, // Inner left
 		 0.25f, 0.5f * float(sqrt(3)) * 1 / 6, 0.0f,     0.0f, 1.0f, 0.0f, // Inner right
-		 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     1.0f, 0.0f, 0.0f  // Inner down
+		 0.0f, -0.5f * float(sqrt(3)) * 1 / 3, 0.0f,     1.0f, 0.0f, 0.0f, // Inner down
 	};
 
 	GLuint indices[] =
@@ -59,7 +60,7 @@ int main()
 		0, 3, 5, // Lower left triangle
 		3, 2, 4, // Lower right triangle
 		5, 4, 1, // Upper triangle
-		3, 4, 5
+		3, 4, 5, // Mid triangle
 	};
 
 	GLuint VAO, VBO, EBO;
@@ -88,18 +89,42 @@ int main()
 	glBindVertexArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
+	float timeElapsed = 0;
+	std::vector<int> fps;
+	
+	auto time = std::chrono::high_resolution_clock::now();
+	int fpsCount = 0;
+
 	while (!glfwWindowShouldClose(window))
 	{
-		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		auto newTime = std::chrono::high_resolution_clock::now();
+		float frameTime = std::chrono::duration<float, std::chrono::seconds::period>(newTime - time).count();
+		time = newTime;
+
+		timeElapsed += frameTime;
+
+		if (timeElapsed >= 1.0f)
+		{
+			std::cout << fpsCount << std::endl;
+			fps.push_back(fpsCount);
+			fpsCount = 0;
+			timeElapsed -= 1;
+		}
+
+		fpsCount++;
+
+		glClearColor(timeElapsed, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(program);
 		glBindVertexArray(VAO);
 
-		glDrawElements(GL_TRIANGLES, sizeof(indices)/sizeof(GLuint), GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(GLuint), GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 		glfwPollEvents();
 	}
+
+	getFPS(fps);
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
@@ -110,6 +135,26 @@ int main()
 	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
+}
+
+void getFPS(std::vector<int> fps)
+{
+	int low = 1000000;
+	int high = 0;
+	int avg = 0;
+
+	for (int i = 0; i < fps.size(); i++)
+	{
+		if (low > fps[i]) low = fps[i];
+		else if (high < fps[i]) high = fps[i];
+
+		avg += fps[i];
+	}
+
+	std::cout << "\n\n-----Result-----\n";
+	std::cout << "LOW  : " << low << "\n";
+	std::cout << "HIGH : " << high << "\n";
+	std::cout << "AVG  : " << float(avg / fps.size()) << "\n";
 }
 
 static void openGLDebugCallback(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char* message, const void* userParam) {
@@ -159,3 +204,4 @@ unsigned int CompileShader(unsigned int type, const std::string& source)
 
 	return id;
 }
+
--- src/Main.h
+++ src/Main.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
+void getFPS(std::vector<int> fps);
 static void openGLDebugCallback(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char* message, const void* userParam);
 unsigned int CompileShader(unsigned int type, const std::string& source);
